772e54b703f8e80479318611bfd6410bf5bcabcb
Boogie <148373295+Boogie231@users.noreply.github.com>
2025-05-07 21:20:18 +0300

    Upgrade code in order to use the data given by the Neural Network

--- ETDK/double_pendulum2.jl
+++ ETDK/double_pendulum2.jl
@@ -6,13 +6,21 @@ begin
 	using RigidBodyDynamics
 	using MeshCatMechanisms
 	using StaticArrays
+    using Flux
+
 	using LinearAlgebra
 	using Rotations
     using GLMakie
     # using Plots
 
+    include("..\\GeneralFunctions\\file_read_write.jl")
+    include("..\\NeuralNetworks\\basic_mine.jl")
+    include("..\\NeuralNetworks\\activation_functions.jl")
+    println("Setup done...")
+
 end
 
+# Start MeshCat simulation:
 begin
 	urdf = "ETDK/Acrobot_mod.urdf" # modified Acrobot
 	mechanism = parse_urdf(urdf)
@@ -48,6 +56,57 @@ begin
 
 end
 
+# Set up the goal data:
+begin
+    # General data:
+	# used_index = 87
+	used_index = 312
+	
+	# Read in the actual data:
+	goal_path_raw = Read_In("PathFollow\\inputData\\coordinates4.txt"; first_line = true)
+	goal_path = goal_path_raw[1:used_index, 1:2] # csak az elso ket oszlopot venni figyelembe, a z-t ignoráljuk az adatokból
+	
+	# scale data for the robot:
+
+
+    x_central = minimum(goal_path[:, 1]) # find minimum of x data
+	goal_path[:, 1] = goal_path[:, 1] .- x_central
+
+    # norm data
+	norm_scale =  maximum(goal_path[:, 1])
+	goal_path[:, 1] = goal_path[:, 1] ./ norm_scale
+	goal_path[:, 2] = goal_path[:, 2] ./ norm_scale
+    
+    # scale data
+	xy_scale = 1.5
+    goal_path[:, 1] = goal_path[:, 1] .* xy_scale
+	goal_path[:, 2] = goal_path[:, 2] .* xy_scale
+	
+    # translate data
+	x_translation = 0.3 # mm
+	y_translation = (maximum(goal_path[:, 2]) + minimum(goal_path[:, 2])) /2
+	goal_path[:, 1] = goal_path[:, 1] .+ x_translation
+	goal_path[:, 2] = goal_path[:, 2] .- y_translation
+	goal_path[:, 2] = -goal_path[:, 2] # flippeljük az y tengelyt, mert még így maradt a tabletes feldolgozásból az adat
+
+end
+
+goal_x, goal_y = goal_path[:, 1], goal_path[:, 2]
+
+model = deserialize("TestPendulum_Neural\\results\\Flux\\trained_network_Flux0.00166.jls")
+predicted_joints = model(goal_path')'  # Use the model directly for predictions
+
+template_state = MechanismState(mechanism)
+
+converted_qs = [
+    begin
+        qvec = copy(configuration(template_state))  # Get a SegmentedVector copy
+        qvec .= Float64.(predicted_joints[i, :])  # Copy values (convert Float32 → Float64)
+        qvec
+    end
+    for i in 1:size(predicted_joints, 1)
+]
+
 begin
     # Előkészítjük a mechanikai szimulációt
        zero_velocity!(state)
@@ -71,28 +130,38 @@ end
 
 begin
     fig = Figure()
-       ax = Axis(fig[1, 1]; title = "Effector pályája", xlabel = "X", ylabel = "Y",aspect=DataAspect())
+       ax = Axis(fig[1, 1]; title = "Efektor pályája", xlabel = "X", ylabel = "Y",aspect=DataAspect(),
+       titlesize = 38,
+       xlabelsize = 30,
+       ylabelsize = 30,
+       xticklabelsize = 20,
+       yticklabelsize = 20)
        xs = Observable(Float32[])
        ys = Observable(Float32[])
    
        # Pálya kirajzolása vonallal
-       lineplot = lines!(ax, xs, ys, color = :red, linewidth = 2,  label="nyomvonal")
+       lineplot = lines!(ax, xs, ys, color = :red, linewidth = 2,  label="valós nyomvonal")
        lineplot = scatter!(ax, xs, ys, color = :red, linewidth = 2)
+
+       lineplot = lines!(ax, goal_x, goal_y, color = :blue, linewidth = 2,  label="cél nyomvonal")
+
        xlims!(ax, (-2, 2)) 
        ylims!(ax, (-2, 2)) 
+       
    
    # Draw the x-axis (OX)
    lines!(ax, [-2, 2], [0, 0], color=:black, linewidth=2)
    
    # Draw the y-axis (OY)
    lines!(ax, [0, 0], [-2, 2], color=:black, linewidth=2)
-   axislegend(ax)
+   axislegend(ax, labelsize = 22)
+
    
     display(fig)  # Makie ablak megnyitása
-    # sleep(1)
-
+    sleep(1)
+    println("Done Makie...")
     # Animáció futtatása
-    for (i, q) in enumerate(qs)
+    for (i, q) in enumerate(converted_qs)
         set_configuration!(state, q)
 
         # Számoljuk az effektor pozícióját
@@ -103,12 +172,12 @@ begin
         push!(ys[], Float32(ee_pos.v[2]))
 		# println(ee_pos.v[1])
         notify(xs)
-        notify(ys)
+        notify(ys)  
 
         # MeshCat render
         MeshCatMechanisms._render_state!(mvis1, state)
 
-        sleep(0.001)
+        sleep(0.1)
     end
 
     println("Animáció vége")
